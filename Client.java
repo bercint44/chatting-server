1e8636441d01c7c206494b6cf713b57ef0090e33
bercint44 <149394449+bercint44@users.noreply.github.com>
2023-11-06 21:52:56 +0700

    Update client remote server
    Update client remote server

--- Client.java
+++ Client.java
@@ -1,5 +1,3 @@
-// Client.java
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,37 +8,34 @@ import java.net.Socket;
 
 public class Client {
 
-    // Alamat server yang akan dikoneksi
     private static final String SERVER_ADDRESS = "localhost";
-    // Port server yang akan dikoneksi
     private static final int SERVER_PORT = 7890;
 
     public static void main(String[] args) {
-        System.out.println("untuk keluar ketik q");
-        
+        System.out.println("Untuk keluar, ketik 'q'");
+
         try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT)) {
             BufferedReader keyboardReader = new BufferedReader(new InputStreamReader(System.in));
-            
+
             System.out.print("Nickname: ");
             String nickname = keyboardReader.readLine();
-			
 
-            // Membuka input stream dan output stream untuk berkomunikasi dengan server
             InputStream serverInput = socket.getInputStream();
             BufferedReader serverReader = new BufferedReader(new InputStreamReader(serverInput));
             OutputStream serverOutput = socket.getOutputStream();
             PrintWriter serverWriter = new PrintWriter(serverOutput, true);
 
-            // Mengirimkan nickname ke server
             serverWriter.println(nickname);
 
-            // Thread untuk membaca pesan dari server
             Thread readThread = new Thread(() -> {
                 try {
                     String serverMessage;
                     while ((serverMessage = serverReader.readLine()) != null) {
-                        // Menampilkan pesan yang diterima dari server
-                        System.out.println(serverMessage);
+                        if (serverMessage.startsWith("RESPONSE:")) {
+                            System.out.println("Server response: " + serverMessage.substring(9));
+                        } else {
+                            System.out.println(serverMessage);
+                        }
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -50,19 +45,24 @@ public class Client {
 
             String message;
             while (true) {
-                // Membaca pesan dari keyboard
                 message = keyboardReader.readLine();
-				
+
                 if (message == null || message.toLowerCase().equals("q")) {
                     break;
                 }
-					serverWriter.println(message);
+
+                serverWriter.println(message);
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            
+
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
