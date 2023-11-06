ea85c4ad9da4c0adf3965c0d87931911b8782b04
dihakar <144807818+dihakar@users.noreply.github.com>
2023-11-06 23:07:33 +0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -54,7 +54,7 @@ public class Client {
                 serverWriter.println(message);
 
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(50);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
