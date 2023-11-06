ea2201396df7d998cf69851154011b5120f7da72
ALWIN <150051258+bercinta69@users.noreply.github.com>
2023-11-06 22:08:56 +0700

    Client saya update tambahkan ini program client

--- Client.java
+++ Client.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+//ini program client
 public class Client {
 
     private static final String SERVER_ADDRESS = "localhost";
